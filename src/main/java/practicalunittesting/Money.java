f8b717a56bad55ae09949fe3de22a497261b5642
Alassane NIANG <adniang75@gmail.com>
2019-05-21 12:30:05 +0200

    Money class to be tested

---
+++ src/main/java/practicalunittesting/Money.java
@@ -0,0 +1,28 @@
+package practicalunittesting;
+
+public class Money {
+    
+    private final int amount;
+    private final String currency;
+    
+    public Money( int amount, String currency ) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+    
+    public int getAmount() {
+        return this.amount;
+    }
+    
+    public String getCurrency() {
+        return this.currency;
+    }
+    
+    public boolean equals( Object anObject ) {
+        if ( anObject instaceof Money ) {
+            Money money = (Money) anObject;
+            return money.getCurrency().equals(this.getCurrency()) && ( money.getAmount() == this.getAmount() );
+        }
+        return false;
+    }
+}
