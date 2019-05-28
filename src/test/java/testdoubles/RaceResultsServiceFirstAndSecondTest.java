596e6f66a26a669401d6207ffba4e6d9d0d624b7
Alassane NIANG <adniang75@gmail.com>
2019-05-28 18:14:53 +0200

    First attempt

--- src/test/java/testdoubles/RaceResultsServiceFirstAndSecondTest.java
+++ src/test/java/testdoubles/RaceResultsServiceFirstAndSecondTest.java
@@ -5,22 +5,41 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 
-
 public class RaceResultsServiceFirstAndSecondTest {
-    
+
     @Test
     public void subscribedClientShouldReceiveMessage() {
-        
+
         RaceResultsService raceResults = new RaceResultsService();
-        
+
         Client client = mock( Client.class );
         Message message = mock( Message.class );
-        
+
         raceResults.addSubscriber( client );
         raceResults.send( message );
-        
+
         verify( client ).receive( message );
-        
+
+    }
+
+    @Test
+    public void messageShouldBeSentToAllSubscribedClients() {
+
+        RaceResultsService raceResults = new RaceResultsService();
+
+        Client clientA = mock( Client.class, "clientA" );
+        Client clientB = mock( Client.class, "clientB" );
+
+        Message message = mock( Message.class );
+
+        raceResults.addSubscriber( clientA );
+        raceResults.addSubscriber( clientB );
+
+        raceResults.send( message );
+
+        verify( clientA ).receive( message );
+        verify( clientB ).receive( message );
+
     }
 
 }
