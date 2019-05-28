16a1627fd517133fdf0db2a0147f36cf4af9124f
Alassane NIANG <adniang75@gmail.com>
2019-05-28 15:48:14 +0200

    single subscriber receives message

---
+++ src/main/java/testdoubles/Client.java
@@ -0,0 +1,7 @@
+package testdoubles;
+
+public interface Client {
+
+    void receive( Message message );
+
+}
---
+++ src/main/java/testdoubles/RaceResultsService.java
@@ -0,0 +1,15 @@
+package testdoubles;
+
+public class RaceResultsService {
+    
+    private Client client;
+    
+    public void addSubscriber( Client client ) {
+        this.client = client;
+    }
+
+    public void send( Message message ) {
+        client.receive( message );
+    }
+
+}
