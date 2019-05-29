69f3d7a51c08a806f36cb246895cfea2b91eb8f8
Alassane NIANG <adniang75@gmail.com>
2019-05-29 15:22:10 +0200

    the fifth test: unsubscribed client stops receiving messages

---
+++ src/test/java/testdoubles/RaceResultsServiceFirstSecondThirdFourthAndFifthTest.java
@@ -0,0 +1,72 @@
+package testdoubles;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+public class RaceResultsServiceFirstSecondThirdFourthAndFifthTest {
+    
+    private RaceResultsService raceResults = new RaceResultsService();
+    
+    private Message message = mock( Message.class );
+    
+    private Client clientA = mock( Client.class, "clientA" );
+    private Client clientB = mock( Client.class, "clientB" );
+
+    // zero subscribers
+    @Test
+    public void notSubscribedClientShouldReceiveMessage() {
+        
+        raceResults.send( message );
+        
+        verify( clientA, never() ).receive( message );
+        verify( clientB, never() ).receive( message );
+        
+    }
+
+    // one subscriber
+    @Test
+    public void subscribedClientShouldReceiveMessage() {
+
+        raceResults.addSubscriber( clientA );
+        raceResults.send( message );
+
+        verify( clientA ).receive( message );
+
+    }
+
+    // two subscribers
+    @Test
+    public void messageShouldBeSentToAllSubscribedClients() {
+
+        raceResults.addSubscriber( clientA );
+        raceResults.addSubscriber( clientB );
+
+        raceResults.send( message );
+
+        verify( clientA ).receive( message );
+        verify( clientB ).receive( message );
+
+    }
+    
+    // multi subscriber
+    @Test
+    public void shouldSendOnlyOneMessageToMultiSubscriber() {
+        raceResults.addSubscriber( clientA );
+        raceResults.addSubscriber( clientA );
+        raceResults.send( message );
+        verify( clientA ).receive( message );
+    }
+    
+    // remove subscriber
+    @Test
+    public void unsubscribedClientShouldNotReceiveMessage() {
+        raceResults.addSubscriber( clientA );
+        raceResults.removeSubscriber( clientA );
+        raceResults.send( message );
+        verify( clientA, never() ).receive( message );
+    }
+    
+}
